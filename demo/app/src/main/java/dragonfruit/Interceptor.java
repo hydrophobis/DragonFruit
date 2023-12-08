7c8e59eb02425bfc9c053295f10d18d410d925ba
CamoGekko <142858559+CamoGekko@users.noreply.github.com>
2023-12-08 01:05:12 +0000

    Commit

---
+++ demo/app/src/main/java/dragonfruit/Interceptor.java
@@ -0,0 +1,21 @@
+package dragonfruit;
+
+import dragonfruit.App.*;
+import java.net.*;
+import java.util.logging.LogRecord;
+import java.io.IOException;
+
+public class Interceptor{
+
+    public static load(){
+        Socket socket = new Socket();
+        boolean status = java.net.ServerSocket.isConnected();
+        socket.bind("192.86.101", 1085);
+        Socket.accept();
+        Log("Listening");
+
+        if(status == true
+
+        );
+    }
+}
